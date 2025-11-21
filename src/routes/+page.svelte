6e92708ccd8d62f55a3fa2790f48b361acc8059d
Jan Brits <pcerservices@gmail.com>
2025-11-21 14:56:18 +0200

    initial svelte setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<h1>Test page on Svelte</h1>
+<p>Let's see how much easier this is compared to Leptos and Rust</p>
+<script>
+  let count = 0;
+</script>
+
+<h2>Hello SvelteKit 👋</h2>
+
+<button on:click={() => count++}>
+  Count is {count} (click here to make the count increase)
+</button>
+<h3>No usestate nonsense, nice!</h3>
